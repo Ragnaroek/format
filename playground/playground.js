3ca0b66e20111449cddb392be4216f29b461ef71
Michael Bohn <michael.bohn@freiheit.com>
2020-06-22 08:06:52 +0200

    infer param type and show a hint

--- playground/playground.js
+++ playground/playground.js
@@ -1,8 +1,8 @@
 
 function onDirectiveChange (text) {
-  var directives = 0
-  var lastChar = ''
-  for (var i = 0; i < text.length; i++) {
+  let directives = 0
+  let lastChar = ''
+  for (let i = 0; i < text.length; i++) {
     if (text.charAt(i) == '~') {
       if (lastChar == '~') {
         directives--
@@ -13,40 +13,57 @@ function onDirectiveChange (text) {
     lastChar = text.charAt(i)
   }
 
-  var paramElem = document.getElementById('params')
-  var params = paramElem.children
-  var diffCnt = directives - (params.length / 2)
+  let paramElem = document.getElementById('params')
+  let params = paramElem.children
+  let diffCnt = directives - (params.length / 2)
   if (diffCnt < 0) {
     // remove
-    for (var i = diffCnt; i < 0; i++) {
+    for (let i = diffCnt; i < 0; i++) {
       params.item(params.length - 1).remove()
       params.item(params.length - 1).remove()
     }
   } else if (diffCnt > 0) {
     // add
-    for (var i = diffCnt; i > 0; i--) {
+    for (let i = diffCnt; i > 0; i--) {
       paramElem.insertAdjacentHTML('beforeend', '<span>,</span>')
-      paramElem.insertAdjacentHTML('beforeend', '<input type="text" style="display:inline-block">')
+      paramElem.insertAdjacentHTML('beforeend', `<label style="display:inline-block">
+        <span style="position:absolute; top:6.7em; color: silver"><em class="paramType">string</em></span><input type="text" style="display:inline-block" oninput="onParamChange(this)">
+      </label>`)
     }
   }
 }
 
-function format () {
-  var directive = document.getElementById('directive').value
-  var paramsHtml = document.getElementById('params').children
+function onParamChange (inputElem) {
+  let num = Number(inputElem.value)
+  let typeElem = inputElem.previousSibling.children.item(0)
+  if (isNaN(num)) {
+    typeElem.innerText = 'string'
+  } else {
+    typeElem.innerText = 'number'
+  }
+}
 
-  var params = []
+function format () {
+  let directive = document.getElementById('directive').value
+  let paramsHtml = document.getElementById('params').children
+
+  let params = []
   params.push(directive)
 
-  for (var i = 0; i < paramsHtml.length; i++) {
-    var param = paramsHtml.item(i)
-    if (param.tagName == 'INPUT') {
-      params.push(param.value)
+  for (let i = 0; i < paramsHtml.length; i++) {
+    let param = paramsHtml.item(i)
+    if (param.tagName == 'LABEL') {
+      for (let z = 0; z < param.children.length; z++) {
+        let subParam = param.children.item(z)
+        if (subParam.tagName == 'INPUT') {
+          params.push(subParam.value)
+        }
+      }
     }
   }
 
-  var formatted = Sformat.apply(null, params)
-  var resultNode = document.createElement('div')
+  let formatted = Sformat.apply(null, params)
+  let resultNode = document.createElement('div')
   resultNode.insertAdjacentText('beforeend', `format.Sformat("${directive}") => ${formatted}`)
   document.getElementById('output').prepend(resultNode)
 }
